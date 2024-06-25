f619be907c023b293d94323080ad40bfd3dafc16
Samuel Darkhovsky <darkhovsky@usa.net>
2024-06-25 18:29:10 -0400

    sends picture to the server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from 'react';
 import './App.css';
+//import Buffer from 'Buffer'
+
 
 const DEBUGGING_LOCAL = 1;
 const WEB_SERVICE_URL = "https://nvidia-contest-express-web-service.onrender.com";
@@ -11,20 +13,18 @@ if (DEBUGGING_LOCAL)
 
 export default function App() {
     const [imageSrc, setImageSrc] = useState();
-    function handleChange(e) {
+    const [imageFile, setImageFile] = useState();
 
+    function handleChange(e) {
+        setImageFile(e.target.files[0]);
         let url = URL.createObjectURL(e.target.files[0]);
 
-        //console.log(e.target.files[0]);
-        //console.log(url);
         setImageSrc(url);
     }    
 
     function reqListener() {
         let responseElement = document.getElementsByName("response_label");
-        //console.log(responseElement);
         responseElement[0].innerText = this.responseText;
-        //console.log(this.responseText);
     }
 
     function handleSubmit(e) {
@@ -36,24 +36,57 @@ export default function App() {
         const form = e.target;
         const formData = new FormData(form);
 
-        const query = service_url + "/user/message?query=" + formData.get("question_input");
+        const reader = new FileReader();
+        reader.onloadend = (e) => {
+            //console.log("e.target.result:", e.target.result);
 
-        const req = new XMLHttpRequest();
-        req.addEventListener("load", reqListener);
-        req.open("GET", query);
-        req.send();
+            //const image_array = new Uint8Array(e.target.result);
+            //const buffer_array = Buffer.from(image_array);
+            //const imageB64 = Buffer.from(image_array).toString('base64');
 
-        // Or you can work with it as a plain object:
-        //const formJson = Object.fromEntries(formData.entries());
-        //console.log(formJson);
+            const query = service_url + "/user/message?query=" + formData.get("question_input");
+            const req = new XMLHttpRequest();
+            req.open("POST", query, true);     
+            // the type is in imageFile.type
+            // nvidia-contest_express-web-service/app.js currently accepts only imageFile.type "image/jpeg"
+            req.setRequestHeader('Content-Type', imageFile.type);            
+            req.addEventListener("load", reqListener);
+            req.onreadystatechange = () => {
+              if (req.readyState === 4 && (req.status === 200 || req.status === 201) ) {
+                console.log("req.response:", req.response);
+              }
+            }
+            /* it seems the send's argument can be:
+                imageFile: File { name: "sample_image.jpeg", lastModified: 1718588201289, webkitRelativePath: "", size: 52604, type: "image/jpeg" }
+                e.target.result: ArrayBuffer { byteLength: 52604 }
+                image_array: Uint8Array(52604) [ 255, 216, 255, 224, 0, 16, 74, 70, 73, 70, … ]
+                buffer_array: Array(52604) [ 255, 216, 255, 224, 0, 16, 74, 70, 73, 70, … ]
+                The server seems to detect in all cases, but buffer_array:
+                      [Symbol(kHeaders)]: {
+                          accept: 'star/star',
+                          'content-type': 'image/jpeg',
+                          'content-length': '52604',
+                          
+                       rawHeaders: [
+                          'Content-Type',
+                          'image/jpeg',
+                          'Content-Length',
+                          '52604',                
+                How can it be?
+            */
+            req.send(e.target.result);
+        };
+        reader.readAsArrayBuffer(imageFile);
     }
 
 
+    // nvidia-contest_express-web-service/app.js currently accepts only imageFile.type "image/jpeg"
+    //             <input type="file" accept=".jpg, .jpeg, .png" onChange={handleChange} />
     return (
         <form className="App" onSubmit={handleSubmit}>
           <div>
             <h2>Select an Image:</h2>
-            <input type="file" accept=".jpg, .jpeg, .png" onChange={handleChange} />
+            <input type="file" accept=".jpeg" onChange={handleChange} />
             <img src={imageSrc} alt=""/>
           </div>
           <div>
@@ -62,7 +95,7 @@ export default function App() {
               </label>
           </div>
           <div>
-              <label name="response_label">
+              <label name="response_label" >
                 Response will be placed here...
               </label>
           </div>
